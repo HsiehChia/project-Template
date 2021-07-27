993fab9c0456c40b12e4ef456ec4eadb24c2dd49
Jane <jane.xie@bundleb2b.net>
2021-07-27 15:05:25 +0800

    feat: write a server demo in simple-pro

--- simple-pro/src/pages/Login/Login.js
+++ simple-pro/src/pages/Login/Login.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import base from '../../service'
 
 export default function Login() {
+    const getUser = base.getUser
+    console.log(getUser())
     return (
         <div>
             Login
---
+++ simple-pro/src/utils/url.js
@@ -0,0 +1,5 @@
+const url = {
+    baseUrl: "http://127.0.0.1:3000",
+}
+
+export default url
