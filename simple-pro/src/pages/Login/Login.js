4b0533940ba5104c07346a80d5595f50010509cf
Jane <jane.xie@bundleb2b.net>
2021-07-28 17:48:16 +0800

    feat: init page components

--- simple-pro/src/pages/Login/Login.js
+++ simple-pro/src/pages/Login/Login.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import userRequest from '../../service'
+// import userRequest from '../../service'
+import { Header, Footer } from '../../utils/Components'
 
 export default function Login() {
-    const getUser = () => userRequest.getUser()
-    getUser()
+    // const getUser = () => userRequest.getUser()
+    // getUser()
     return (
-        <div>
+        <>
+            <Header></Header>
+            <Footer></Footer>
             Login
-        </div>
+        </>
     )
 }
