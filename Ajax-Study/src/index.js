15af83ff32cca4110e1c4d56dcb3c66a0ca574a6
Jane-Xie <jane.xie@bundleb2b.net>
2021-09-24 14:08:12 +0800

    feat: bundle api call

--- Ajax-Study/src/index.js
+++ Ajax-Study/src/index.js
@@ -8,11 +8,14 @@ window.b3utils = b3utils
 
 export default b3utils
 
-const baseUrl = 'http://localhost:8000'
+const baseUrl = 'https://dev-v2.bundleb2b.net'
 
 function TestData() {
-  b3utils.api.user.getUserApi(baseUrl)
-  b3utils.api.user.fetchUserApi(baseUrl)
+  // b3utils.api.user.getUserApi(baseUrl)
+  // b3utils.api.user.b3fetchUserApi(baseUrl)
+  // b3utils.api.user.fetchUserApi(baseUrl)
+  b3utils.api.user.getBCToken(baseUrl + '/customer/current.jwt')
+  b3utils.api.user.getBCTokenByAxios(baseUrl + '/customer/current.jwt')
 }
 
 TestData()
