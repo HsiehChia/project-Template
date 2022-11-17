24d64978a54b5631f6fbbc6a44313bf9f3c19060
JaneXieCd <jane.xie@silksoftware.com>
2022-11-17 15:19:33 +0800

    feat: less and dva config

--- UmiJS-Study/src/pages/index.tsx
+++ UmiJS-Study/src/pages/index.tsx
@@ -5,6 +5,9 @@ export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <div className={styles.test}>
+        test size
+      </div>
       <Link to='/home'>Home</Link>
     </div>
   );
