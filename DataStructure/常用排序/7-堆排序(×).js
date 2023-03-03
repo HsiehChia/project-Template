dd865e88eb99aafd014116907787f7b79707182f
HsiehChia <3226846895@qq.com>
2023-03-03 17:25:08 +0800

    feat: 补充

--- DataStructure/常用排序/7-堆排序(×).js
+++ DataStructure/常用排序/7-堆排序(×).js
@@ -1,6 +1,7 @@
 void function() {
   const arr = [3, 2, 1, 5, 6, 4];
 
+  // 主要运用队列
   function heapSort(arr) {
     if(arr.length <= 1) {
       return arr;
--- DataStructure/常用遍历/3-层序.js
+++ DataStructure/常用遍历/3-层序.js
@@ -1,1 +1,23 @@
-void function() {}()
+void function() {
+  const arr = [-10,9,20,null,null,15,7]
+  // 构造二叉树
+  function binaryTree(arr) {
+    const root = {}
+    if(arr.length <= 1) {
+      root.value = arr[0]
+      root.left = null;
+      root.right = null;
+      return root
+    }
+    // 利用队列
+    root.value = arr.shift();
+
+    console.log(root, arr)
+
+
+
+    return arr
+  }
+
+  binaryTree(arr);
+}()
